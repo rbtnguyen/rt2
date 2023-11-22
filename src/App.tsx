8290d95101ed9ae497f644db12505abff9cc85d1
Binh Nguyen <rbtnguyen@gmail.com>
2023-11-22 08:36:53 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <h1>Pill Component</h1> */}
+      <section className="pills-container">
+        <div className="pills-row">
+          <div className="pills-group">
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 1a</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 2</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 3</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 4</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 5</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 6</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 7</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 8</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 9</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 10</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 11</div>
+            {/* Duplicating */}
+          </div>
+          <div className="pills-group">
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 1b</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 2</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 3</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 4</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 5</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 6</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 7</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 8</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 9</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 10</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 11</div>
+            {/* Duplicating */}
+          </div>
+        </div>
+        <div className="pills-row">
+          <div className="pills-group">
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 1a</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 2</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 3</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 4</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 5</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 6</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 7</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 8</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 9</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 10</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 11</div>
+            {/* Duplicating */}
+          </div>
+          <div className="pills-group">
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 1b</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 2</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 3</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 4</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 5</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 6</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 7</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 8</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 9</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 10</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 11</div>
+            {/* Duplicating */}
+          </div>
+        </div>
+        <div className="pills-row">
+          <div className="pills-group">
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 1a</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 2</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 3</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 4</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 5</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 6</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 7</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 8</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 9</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 10</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 11</div>
+            {/* Duplicating */}
+          </div>
+          <div className="pills-group">
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 1b</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 2</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 3</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 4</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 5</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 6</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 7</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 8</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 9</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 10</div>
+            <div className="pill" tabIndex={0}><img src="https://i.imgur.com/fZBXVu4.png" />Stripe 11</div>
+            {/* Duplicating */}
+          </div>
+        </div>
+
+      </section>
+    </div>
+  );
+}
